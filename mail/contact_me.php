917b7b73bdef1149bac2dad88cb0c43d46f1f909
AMARAL Mateus <mateus.amaral@alstomgroup.com>
2024-11-18 10:34:42 -0300

    revert template to scratch

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'mateus.amaral.dev@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Formulário de Contato do Site Pessoal:  $name";
-$email_body = "Você recebeu um novo e-mail do seu formulário de cotato do site\n\n"."Aqui estão detalhes:\n\n\Nome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensage:\n$message";
-$headers = "From: mateus.amaral.dev@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'jerome.lachaud@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
